775d27de99169d66d9276547dbba05cc7c2069ca
jmanero <john.manero@gmail.com>
2015-05-01 01:56:02 -0400

    Lint

--- lib/thor_scmversion.rb
+++ lib/thor_scmversion.rb
@@ -1,14 +1,18 @@
 require 'thor-scmversion'
 
+##
+# Make the `current` task behave the way I think it should.
+##
 class ThorSCMVersion::Tasks
   method_option :version_file_path,
-    :type => :string,
-    :default => nil,
-    :desc => "An additional path to copy a VERSION file to on the file system."
-  desc "current", "Show current SCM tagged version"
+                :type => :string,
+                :default => nil,
+                :desc => 'An additional path to copy a VERSION file to on the file system.'
+
+  desc 'current', 'Show current SCM tagged version'
   def current
     write_version(options[:version_file_path])
-    say_status :version, "Current #{ current_version.to_s }"
+    say_status :version, "Current #{ current_version }"
 
     current_version.to_s
   end
